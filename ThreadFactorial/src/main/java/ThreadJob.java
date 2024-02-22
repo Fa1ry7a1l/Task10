43a3573f356e46b1b11681c90b044b01954bbb04
Андрей Сушко <46302763+Fa1ry7a1l@users.noreply.github.com>
2024-02-22 17:41:43 +0300

    Initial commit

---
+++ ThreadFactorial/src/main/java/ThreadJob.java
@@ -0,0 +1,47 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+public class ThreadJob implements Runnable {
+
+
+    private Semaphore semaphore;
+    private Scanner scanner;
+
+    public ThreadJob(Semaphore sem, Scanner scanner) {
+        this.semaphore = sem;
+        this.scanner = scanner;
+    }
+
+    private BigInteger factorial(int n) {
+        var res = BigInteger.ONE;
+        for (int i = 1; i < n; i++) {
+            res = res.multiply(BigInteger.valueOf(i));
+        }
+        return res;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            int value;
+            try {
+                semaphore.acquire();
+                if (!scanner.hasNextInt()) {
+                    //признак того, что мы досчитали до конца файла
+                    break;
+                }
+                value = scanner.nextInt();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            finally {
+                semaphore.release();
+            }
+            var factVal = factorial(value);
+            System.out.printf("f(%d) = %s\n",value,factVal.toString());
+        }
+        System.out.println("поток " + Thread.currentThread().getName() + " завершился");
+
+    }
+}
